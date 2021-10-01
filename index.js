1d00a62af5d36707ddf3abd06ee441df25c9e79b
taiyu <taiyu8705@gmail.com>
2021-10-01 12:21:31 +0800

    20211001

---
+++ index.js
@@ -0,0 +1,22 @@
+let des = new Date(2021, 9, 1, 22, 0, 0)
+
+function updateTime() {
+    let cur = new Date();
+    let td = des - cur
+
+    if (td < 0) {
+        return;
+    }
+
+    let _days = Math.floor(td / 1000 / 24 / 60 / 60)
+    let _hr = Math.floor((td / 1000 / 60 / 60) % 24)
+    let _min = Math.floor((td / 1000 / 60) % 60)
+    let _sec = Math.floor((td / 1000) % 60)
+
+    document.getElementById("days").innerHTML = _days;
+    document.getElementById("hr").innerHTML = _hr;
+    document.getElementById("mins").innerHTML = _min;
+    document.getElementById("sec").innerHTML = _sec;
+}
+
+let intervalID = setInterval(updateTime, 1000);
